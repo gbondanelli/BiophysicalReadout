4ab2fb715be9b3636adf59a7f8dc1e2d86adc809
Giulio Bondanelli <giulio.bondanelli@gmail.com>
2021-03-13 13:48:17 +0100

    Modified pearson corr code

---
+++ Pearon_correlations/plot_pearsoncorr.py
@@ -0,0 +1,83 @@
+import sys
+base_directory = '/Users/giuliobondanelli/OneDrive - Fondazione Istituto Italiano Tecnologia/Code/code_Valente21_to_share'
+sys.path.insert(0,base_directory + '/modules_')
+from numpy import *
+from encdec import *
+from stattools import *
+from information import *
+from plottingtools import *
+from managingtools import *
+from lif import *
+from matplotlib.pyplot import *
+import rc_parameters
+from scipy.stats import ttest_ind
+
+##
+
+correlations_corr_s1 =load(base_directory + '/data/data_pearson_correlations/correlations_corr_s1.npy')
+correlations_corr_s2 =load(base_directory + '/data/data_pearson_correlations/correlations_corr_s2.npy')
+correlations_err_s1 =load(base_directory + '/data/data_pearson_correlations/correlations_err_s1.npy')
+correlations_err_s2 =load(base_directory + '/data/data_pearson_correlations/correlations_err_s2.npy')
+
+n_div = correlations_corr_s1.shape[2]
+
+# average over time lags
+
+correlations_corr_s1_ = nanmean(correlations_corr_s1,1)
+correlations_corr_s2_ = nanmean(correlations_corr_s2,1)
+correlations_err_s1_ = nanmean(correlations_err_s1,1)
+correlations_err_s2_ = nanmean(correlations_err_s2,1)
+
+##
+n =2
+# average over stimuli
+vcorr = (correlations_corr_s1_[:,:n]+correlations_corr_s2_[:,:n])/2
+verr = (correlations_err_s1_[:,:n]+correlations_err_s2_[:,:n])/2
+
+# average over subsamplings
+mcorr = nanmean(vcorr,0)
+merr = nanmean(verr,0)
+scorr = nanstd(vcorr,0)
+serr = nanstd(verr,0)
+
+pvalues = ttest_ind(vcorr,verr)[1]
+
+tau_sampling = 1
+bin = int(1000*tau_sampling/n_div)
+fig,ax = subplots(figsize=(1.7,1.9))
+
+pars={'ls':'.-','lw':1.5,'markersize':5, 'err':'sd'}
+my_fill_between(bin*arange(n), [vcorr,verr], ['#4181a6','#d64f7e'], ['#c7dcff','#ffabb3'], ['Correct','Error'],  **pars)
+
+ylimmax = ax.get_ylim()[1]
+for i in range(len(pvalues)):
+    if mcorr[i]>merr[i] and pvalues[i]<0.001:
+        ax.plot([bin*arange(n)[i]],[ylimmax],'x',color='#b5b0b1',markersize=3)
+# ax.spines['left'].set_bounds(0, .1)
+ax.spines['bottom'].set_bounds(0, 400)
+xticks([0,200,400])
+ax.set_xlabel('Lag (ms)')
+ax.set_ylabel('Pearson corr.')
+# legend(frameon=0,loc=3)
+tight_layout()
+# savefig('../../Valente_NN/figs/correlations_correct_error.pdf',transparent=True)
+
+##
+i_lag = 1
+data = [vcorr[:,i_lag],verr[:,i_lag]]
+labels = ['Correct','Error']
+facecolor = ['#4181a6','#d64f7e']
+colorwhisk = ['#4181a6','#4181a6','#d64f7e','#d64f7e']
+colorcaps = colorwhisk
+colorfliers = 'grey'
+width = .8
+ax = my_boxplot((1.2,1.9), data, labels, 40, facecolor, colorwhisk, colorcaps, colorfliers, width)
+# y1 = 0.06
+# y2=0.08
+# ax.spines['left'].set_bounds(y1, y2)
+# ax.set_yticks([y1, y2])
+ax.spines['bottom'].set_bounds(1, 2)         
+tight_layout()
+print('p = ', ttest_ind(vcorr[:,i_lag],verr[:,i_lag])[1], 't = ', ttest_ind(vcorr[:,i_lag],verr[:,i_lag])[0])
+
+
--- modules_/plottingtools.py
+++ modules_/plottingtools.py
@@ -7,30 +7,33 @@ def my_fill_between(x, F, col, colfill, labels,  **pars):
     ls = pars['ls']
     lw = pars['lw']
     ms = pars['markersize']
-    m  = [mean(f,0) for f in F]
-    s  = [std(f,0) for f in F]
+    m  = [nanmean(f,0) for f in F]
+    s  = [nanstd(f,0) for f in F]
     ntrials = F[0].shape[0]
     a = sqrt(ntrials) if pars['err'] == 'se' else 1
     for i in range(len(m)):
         plot(x, m[i], ls, lw = lw, color = col[i], label = labels[i],markersize=ms)
         fill_between(x, m[i]-s[i]/a, m[i]+s[i]/a,color = colfill[i])
 
-def my_boxplot(figsize, data, labels, rotation):
+def my_boxplot(figsize, data, labels, rotation, facecolor, colorwhisk, colorcaps, colorfliers, width):
     fig = figure(figsize=figsize)
     ax  = fig.add_subplot(1, 1, 1)
-    col = 'k'
-    flierprops      = dict(marker = 'o', markerfacecolor = col, markersize = 4, markeredgewidth = .3, linestyle = 'none')
-    whiskerprops    = dict(linewidth=1., color=col)
-    boxprops        = dict(linewidth = 1., color = col)
-    capprops        = dict(linewidth = 1., color = col)
-    bp = ax.boxplot(data, flierprops = flierprops, widths = .35, patch_artist = True,
-                    whiskerprops = whiskerprops, boxprops = boxprops, capprops = capprops)
-    for box in bp['boxes']:
-        box.set(facecolor='w', ) 
+    flierprops      = dict(marker = 'o', markerfacecolor = colorfliers, markersize = 3, markeredgewidth = 0, linestyle = 'none')
+    boxprops        = dict(linewidth = 1.)
+    capprops        = dict(linewidth = 1.)
+    bp = ax.boxplot(data, flierprops = flierprops, widths = width, patch_artist = True,
+                     boxprops = boxprops, capprops = capprops)
+    for i,box in enumerate(bp['boxes']):
+        box.set(facecolor=facecolor[i], color=facecolor[i])
+    for i,whisker in enumerate(bp['whiskers']):
+        whisker.set(color = colorwhisk[i])
+    for i, cap in enumerate(bp['caps']):
+        cap.set(color = colorcaps[i])
     for median in bp['medians']:
-        median.set(color=col, linewidth=.8)
-    xticks(arange(1,len(labels)+1,1), labels, rotation=rotation)
+        median.set(color='grey', linewidth=.8)
+    xticks(arange(1,len(labels)+1,1), labels, rotation=rotation, fontsize=10)
     tight_layout()
+    return ax
 
 
 def define_colormap(colors, N):
